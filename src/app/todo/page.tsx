a7271f046fd4c8f99c2b03541450adf183007001
nkkn-prog <nkkn.prog@gmail.com>
2024-12-19 08:23:28 +0900

    feat:一覧ページの作成

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import styles from '../styles/list.module.css'
 
-const page = () => {
+const listTitle = 'TodoList'
+
+const TodoList = () => {
   return (
-    <div>Index Page</div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <h1>{listTitle}</h1>
+        </div>
+        <div className={styles.main}>
+          <div>あ</div>
+          <div>あ</div>
+          <div>あ</div>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default page
+export default TodoList
